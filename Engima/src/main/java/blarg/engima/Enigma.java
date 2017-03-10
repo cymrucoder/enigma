7e4c62fd64b7f2c1a0abd1cd188fd26e05ad5b7d
cymrucoder <cymrucoder@users.noreply.github.com>
2017-03-10 19:26:28 +0000

    Add swapping rotors.

--- Engima/src/main/java/blarg/engima/Enigma.java
+++ Engima/src/main/java/blarg/engima/Enigma.java
@@ -7,22 +7,43 @@ package blarg.engima;
 public class Enigma {
     
     // Ugly implementation with letters
-    String rotor1 = "EKMFLGDQVZNTOWYHXUSPAIBRCJ";
-    String rotor2 = "AJDKSIRUXBLHWTMCQGZNPYFVOE";
-    String rotor3 = "BDFHJLCPRTXVZNYEIWGAKMUSQO";
-    String reflectorB = "YRUHQSLDPXNGOKMIEBFZCWVJAT";
+    private static final String ROTOR_ONE = "EKMFLGDQVZNTOWYHXUSPAIBRCJ";
+    private static final String ROTOR_TWO = "AJDKSIRUXBLHWTMCQGZNPYFVOE";
+    private static final String ROTOR_THREE = "BDFHJLCPRTXVZNYEIWGAKMUSQO";
     
-    int posRot1;
-    int posRot2;
-    int posRot3;
+    private String[] rotors = new String[3];
+    
+    private String rotor1;
+    private String rotor2;
+    private String rotor3;
+    private String reflectorB = "YRUHQSLDPXNGOKMIEBFZCWVJAT";
+    
+    private int posRot1;
+    private int posRot2;
+    private int posRot3;
     
     public Enigma() {
         posRot1 = 0;
         posRot2 = 0;
         posRot3 = 0;
+        
+        rotor1 = ROTOR_ONE;
+        rotor2 = ROTOR_TWO;
+        rotor3 = ROTOR_THREE;
+        
+        // This is ugly
+        rotors[0] = ROTOR_ONE;
+        rotors[1] = ROTOR_TWO;
+        rotors[2] = ROTOR_THREE;
     }
     
-    public void setRotorPositions(int one, int two, int three) {
+    public void setRotors(int left, int middle, int right) {
+        rotor1 = rotors[left - 1];
+        rotor2 = rotors[middle - 1];
+        rotor3 = rotors[right - 1];
+    }
+    
+    public void setRotorRotations(int one, int two, int three) {
         posRot1 = one;
         posRot2 = two;
         posRot3 = three;
--- Engima/src/test/java/blarg/engima/EnigmaTest.java
+++ Engima/src/test/java/blarg/engima/EnigmaTest.java
@@ -18,7 +18,7 @@ public class EnigmaTest {
     // Reflector B, rotors I, II, III, code A, A, A
     @Test
     public void testEncrypt_whenRotorsAAA_shouldReturnCiphertext() {
-        enigma.setRotorPositions(('A' - 'A'), ('A' - 'A'), ('A' - 'A'));
+        enigma.setRotorRotations(('A' - 'A'), ('A' - 'A'), ('A' - 'A'));
         String plaintext = "AAAA";
         String expectedCiphertext = "BDZG";
         assertEquals(expectedCiphertext, enigma.encrypt(plaintext));
@@ -27,7 +27,7 @@ public class EnigmaTest {
     // Reflector B, rotors I, II, III, code E, H, K
     @Test
     public void testEncrypt_whenRotorsEHK_shouldReturnCiphertext() {
-        enigma.setRotorPositions(('E' - 'A'), ('H' - 'A'), ('K' - 'A'));
+        enigma.setRotorRotations(('E' - 'A'), ('H' - 'A'), ('K' - 'A'));
         String plaintext = "AAAA";
         String expectedCiphertext = "OQNU";
         assertEquals(expectedCiphertext, enigma.encrypt(plaintext));
@@ -36,7 +36,7 @@ public class EnigmaTest {
     // Reflector B, rotors I, II, III, code T, B, C
     @Test
     public void testEncrypt_whenRotorsTBC_shouldReturnCiphertext() {
-        enigma.setRotorPositions(('T' - 'A'), ('B' - 'A'), ('C' - 'A'));
+        enigma.setRotorRotations(('T' - 'A'), ('B' - 'A'), ('C' - 'A'));
         String plaintext = "AAAA";
         String expectedCiphertext = "NMIQ";
         assertEquals(expectedCiphertext, enigma.encrypt(plaintext));
@@ -45,7 +45,7 @@ public class EnigmaTest {
     // Reflector B, rotors I, II, III, code A, A, X
     @Test
     public void testEncrypt_whenRightRotorRollsover_shouldReturnCiphertext() {
-        enigma.setRotorPositions(('A' - 'A'), ('A' - 'A'), ('X' - 'A'));
+        enigma.setRotorRotations(('A' - 'A'), ('A' - 'A'), ('X' - 'A'));
         String plaintext = "AAAA";
         String expectedCiphertext = "BOXY";
         assertEquals(expectedCiphertext, enigma.encrypt(plaintext));
@@ -54,7 +54,7 @@ public class EnigmaTest {
     // Reflector B, rotors I, II, III, code A, Z, X
     @Test
     public void testEncrypt_whenTwoRotorsRollover_shouldReturnCiphertext() {
-        enigma.setRotorPositions(('A' - 'A'), ('Z' - 'A'), ('X' - 'A'));
+        enigma.setRotorRotations(('A' - 'A'), ('Z' - 'A'), ('X' - 'A'));
         String plaintext = "AAAA";
         String expectedCiphertext = "KGPE";
         assertEquals(expectedCiphertext, enigma.encrypt(plaintext));
@@ -63,9 +63,39 @@ public class EnigmaTest {
     // Reflector B, rotors I, II, III, code Z, Z, X
     @Test
     public void testEncrypt_whenThreeRotorsRollover_shouldReturnCiphertext() {
-        enigma.setRotorPositions(('Z' - 'A'), ('Z' - 'A'), ('X' - 'A'));
+        enigma.setRotorRotations(('Z' - 'A'), ('Z' - 'A'), ('X' - 'A'));
         String plaintext = "AAAA";
         String expectedCiphertext = "XXUB";
+        assertEquals(expectedCiphertext, enigma.encrypt(plaintext));
+    }
+    
+    // Reflector B, rotors I, III, II, code A, A, A
+    @Test
+    public void testEncypt_whenTwoRotorsSwapped_shouldReturnCiphertext() {
+        enigma.setRotors(1, 3, 2);
+        enigma.setRotorRotations(('A' - 'A'), ('A' - 'A'), ('A' - 'A'));
+        String plaintext = "AAAA";
+        String expectedCiphertext = "TQUK";
+        assertEquals(expectedCiphertext, enigma.encrypt(plaintext));
+    }
+    
+    // Reflector B, rotors I, III, II, code A, A, A
+    @Test
+    public void testEncypt_whenThreeRotorsSwapped_shouldReturnCiphertext() {
+        enigma.setRotors(3, 1, 2);
+        enigma.setRotorRotations(('A' - 'A'), ('A' - 'A'), ('A' - 'A'));
+        String plaintext = "AAAA";
+        String expectedCiphertext = "CNBE";
+        assertEquals(expectedCiphertext, enigma.encrypt(plaintext));
+    }
+    
+    // Reflector B, rotors I, III, II, code A, A, A
+    @Test
+    public void testEncypt_whenThreeRotorsSwappedAndThreeRollovers_shouldReturnCiphertext() {
+        enigma.setRotors(3, 1, 2);
+        enigma.setRotorRotations(('Z' - 'A'), ('Z' - 'A'), ('X' - 'A'));
+        String plaintext = "AAAA";
+        String expectedCiphertext = "ZUJC";
         assertEquals(expectedCiphertext, enigma.encrypt(plaintext));
     }
 }
