795e951ed98dbdf0f145b38822ba6df0c565eeb1
cymrucoder <cymrucoder@users.noreply.github.com>
2017-04-29 12:35:27 +0100

    Trying new of of working out how to shift values during ciphering. Task #5 - Rotors should be settable - choose 3 from 5.

--- Engima/src/main/java/blarg/engima/Rotor.java
+++ Engima/src/main/java/blarg/engima/Rotor.java
@@ -1,5 +1,8 @@
 package blarg.engima;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author cymrucoder
@@ -7,17 +10,50 @@ package blarg.engima;
 public class Rotor {
     private final String cipher;
     private int position;
+    List<Integer> shifts;
+    
+    /*
+    
+    ABCDEFGHIJKLMNOPQRSTUVWXYZ
+    BDFHJLCPRTXCZNYEIWGAKMUSQO
+    
+    //Position 0 (A)
+    getLFR(0) = 1
+    getLFR(1) = 3
+    getRFL(0) = 19
+    getRFL(1) = 0
+    
+    //Position 1 (B)
+    gLFR(0) = 2
+    gLFR(1) = 4
+    gRFL(0) = 25
+    gRFL(1) = 5
+    
+    */
     
     public Rotor(String cipher) {
-        this.cipher = cipher;
+        this.cipher = cipher.toUpperCase();
+        shifts = new ArrayList<>();
+        
+        char plaintext = 'A';
+        
+        // So BDFHJLC...
+        // First char is B, plaintext A, B-A -> 1
+        // At C, plaintext is G, C-G -> -4
+        
+        for (char ch : cipher.toCharArray()) {
+            shifts.add(ch - plaintext);
+            plaintext++;
+        }
+        
         position = 0;
     }
     
-    int getLeftForRight(int rightIndex) {
+    public int getLeftForRight(int rightIndex) {
         return 0;
     }
     
-    int getRightForLeft(int leftIndex) {
+    public int getRightForLeft(int leftIndex) {
         return 0;
     }
     
@@ -26,7 +62,7 @@ public class Rotor {
      * E.g., position 26 rolls over to 0, position -1 rolls back to 25
      * @param position 
      */
-    void setPosition(int position) {
+    public void setPosition(int position) {
         this.position = position % cipher.length();
     }
     
@@ -34,7 +70,7 @@ public class Rotor {
      * Rotates the rotor one position.
      * @return true if this should trigger the next rotor to rotate.
      */
-    boolean rotate() {
+    public boolean rotate() {
         return false;
     }
 }
